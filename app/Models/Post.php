1063b1ff458368a8074e95ca382b11f57f3f8753
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-07 15:22:08 +0200

    create post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,11 +1,11 @@
 <?php
 
-// namespace App\Models;
+namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-// class Post extends Model
-// {
-//     use HasFactory;
-// }
+class Post extends Model
+{
+    use HasFactory;
+}
