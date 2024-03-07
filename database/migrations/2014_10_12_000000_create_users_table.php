e56870189005ef99db57dc0a35d5438a32bf4816
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-07 15:24:12 +0200

    set up users table structure

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,16 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            // $table->id();
-            // $table->string('full_name');
-            // $table->string('name');
-            // $table->string('location');
-            // $table->string('avatar_url')->default();
-            // $table->string('email')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
-            // $table->string('password');
-            // $table->rememberToken();
-            // $table->timestamps();
+            $table->id();
+            $table->string('full_name');
+            $table->string('name');
+            $table->string('location');
+            $table->string('avatar_url')->default();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
