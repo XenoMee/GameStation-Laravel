4074fa3827889fa128f5b2a4ce10a6c610597247
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-03-12 18:32:47 +0200

    rename route for displaying all posts to '/feed'

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 // PostController Routes
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/feed', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
 Route::get('/posts/{id}/edit', [PostController::class, 'edit']);
